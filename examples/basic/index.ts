b8662654719d9c2b7867bd274932e478739a2102
Jakub Pietras <jakub.pietras@outlook.com>
2022-01-30 19:25:41 +0100

    Update basic example

--- examples/basic/index.ts
+++ examples/basic/index.ts
@@ -1,10 +1,14 @@
 import { createServer } from 'http';
 
-import { jsonResult, mount } from 'lacing';
+import { jsonResult, mount, ProcessedRequest } from 'lacing';
 
 const logger = { error: console.error, info: console.log, warn: console.warn };
 
-const app = async () => jsonResult({ body: { result: 'Ok' }, statusCode: 200 });
+const app = async (request: ProcessedRequest) => {
+  console.log(request);
+
+  return jsonResult({ body: { result: 'ok' }, statusCode: 200 });
+};
 
 createServer(mount(app, { logger })).listen(3000, () => {
   console.log('Listening on 3000...');
