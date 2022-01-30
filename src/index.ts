0dccb2ec7b79951edd29cce5b9934fb8c075728a
Jakub Pietras <jakub.pietras@outlook.com>
2022-01-30 19:19:28 +0100

    Expose new API elements

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { mount } from './mount';
+export { HttpResult } from './httpResult';
+export { jsonResult } from './results/jsonResult';
+export { permanentRedirectionResult } from './results/permanentRedirectionResult';
+export { temporaryRedirectionResult } from './results/temporaryRedirectionResult';
+export { mount, MountOptions } from './mount';
