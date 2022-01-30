56aa46335d1b287f745ea06b29f3fb57f16f3872
Jakub Pietras <jakub.pietras@outlook.com>
2022-01-30 21:18:55 +0100

    Add method routing to basic example

--- examples/basic/index.ts
+++ examples/basic/index.ts
@@ -1,14 +1,12 @@
 import { createServer } from 'http';
 
-import { jsonResult, mount, ProcessedRequest } from 'lacing';
+import { jsonResult, methods, mount } from 'lacing';
 
 const logger = { error: console.error, info: console.log, warn: console.warn };
 
-const app = async (request: ProcessedRequest) => {
-  console.log(request);
-
-  return jsonResult({ body: { result: 'ok' }, statusCode: 200 });
-};
+const app = methods({
+  GET: async () => jsonResult({ body: { result: 'ok' }, statusCode: 200 }),
+});
 
 createServer(mount(app, { logger })).listen(3000, () => {
   console.log('Listening on 3000...');
