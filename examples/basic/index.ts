58ae2829846ec422d3b3df54b06821432b179f23
Jakub Pietras <jakub.pietras@outlook.com>
2022-01-31 02:49:25 +0100

    Add routing and more logging to basic example

--- examples/basic/index.ts
+++ examples/basic/index.ts
@@ -1,12 +1,30 @@
 import { createServer } from 'http';
+import { andThen, pipe, tap } from 'ramda';
 
-import { jsonResult, methods, mount } from 'lacing';
+import { HttpResult, jsonResult, methods, mount, ProcessedRequest, router, routes } from 'lacing';
 
 const logger = { error: console.error, info: console.log, warn: console.warn };
 
-const app = methods({
-  GET: async () => jsonResult({ body: { result: 'ok' }, statusCode: 200 }),
+const logRequest = tap(async (request: ProcessedRequest) => {
+  logger.info(`${request.method} ${request.url.pathname}`);
 });
+
+const logResult = tap(({ statusCode }: HttpResult) => {
+  logger.info(`=> ${statusCode}`);
+});
+
+const appRouter = router(
+  routes({
+    '/example/:param': routes({
+      '/deep/:deepParam': methods({
+        GET: async ({ cookies, params }) =>
+          jsonResult({ body: { cookies, params }, statusCode: 200 }),
+      }),
+    }),
+  })
+);
+
+const app = pipe(logRequest, appRouter, andThen(logResult));
 
 createServer(mount(app, { logger })).listen(3000, () => {
   console.log('Listening on 3000...');
